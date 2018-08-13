46aa49ce9ed1e807c66f58213724a534055b9f0b
BuildTools <34030@youscope.nl>
2018-08-13 16:26:03 +0200

    Core changes

--- src/main/java/me/fadishawki/bungeemsg/bungee/handlers/message/ChatMessage.java
+++ src/main/java/me/fadishawki/bungeemsg/bungee/handlers/message/ChatMessage.java
@@ -6,7 +6,6 @@ import me.fadishawki.bungeemsg.bungee.handlers.channel.Channel;
 import me.fadishawki.bungeemsg.bungee.handlers.filter.Filter;
 import me.fadishawki.bungeemsg.bungee.handlers.player.BungeePlayer;
 import me.fadishawki.bungeemsg.bungee.handlers.server.BungeeServer;
-import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.json.simple.JSONObject;
 
@@ -14,7 +13,7 @@ public class ChatMessage implements Message.Type {
 
     private String message;
 
-    public ChatMessage(String message){
+    public ChatMessage(String message) {
         this.message = message;
     }
 
@@ -22,23 +21,23 @@ public class ChatMessage implements Message.Type {
     @Override
     public boolean send(Receiver receiver) {
         //TODO: PERHAPS CHANGE SOME & ADD A FORMAT! (MSG: [name] >> [you] : message) (CHANNEL: [name] : message)
-        switch(receiver.getType()){
+        switch (receiver.getType()) {
             case PLAYER: {
                 BungeePlayer player = (BungeePlayer) receiver;
-                player.getPlayer().sendMessage(ChatMessageType.CHAT, new TextComponent(message));
+                player.getPlayer().sendMessage(new TextComponent(message));
                 break;
             }
             case CHANNEL: {
                 Channel channel = (Channel) receiver;
-                for(BungeePlayer player : channel.getPlayers()){
-                    player.getPlayer().sendMessage(ChatMessageType.CHAT, new TextComponent(message));
+                for (BungeePlayer player : channel.getPlayers()) {
+                    player.getPlayer().sendMessage(new TextComponent(message));
                 }
                 break;
             }
             case SERVER: {
                 BungeeServer server = (BungeeServer) receiver;
-                for(BungeePlayer player : server.getServerChannel().getPlayers()){
-                    player.getPlayer().sendMessage(ChatMessageType.CHAT, new TextComponent(message));
+                for (BungeePlayer player : server.getServerChannel().getPlayers()) {
+                    player.getPlayer().sendMessage(new TextComponent(message));
                 }
                 break;
             }
